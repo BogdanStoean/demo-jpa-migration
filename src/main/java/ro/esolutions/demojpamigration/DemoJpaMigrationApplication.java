08e4cd602566b3519a8b07e31754609f6e82bd15
Bogdan Stoean <bogdan.stoean@esolutions.ro>
2019-10-23 14:41:25 +0300

    Initial Setup

---
+++ src/main/java/ro/esolutions/demojpamigration/DemoJpaMigrationApplication.java
@@ -0,0 +1,13 @@
+package ro.esolutions.demojpamigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJpaMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/esolutions/demojpamigration/DemoJpaMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.esolutions.demojpamigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
